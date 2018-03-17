771c0ba8a1ae00fdb6c59b334ed998b7674e011c
Kacper Pikulski <pikulak1@gmail.com>
2018-03-17 04:50:29 +0100

    Add form & change h1 animation

--- static/js/app/home.js
+++ static/js/app/home.js
@@ -11,7 +11,7 @@ app.home = {
         animateElements("#home h1 span:nth-of-type(1)", "bounceInDown", 200, "visible");
         animateElements("#home h1 span:nth-of-type(2)", "bounceInDown", 800, "visible");
         animateElements("#home h1 span:nth-of-type(3)", "bounceInDown", 1000, "visible highlight");
-        animateElements("#home h1 span:nth-of-type(4)", "bounceInUp", 2200, "visible");
+        animateElements("#home h1 span:nth-of-type(4)", "bounceInDown", 2200, "visible");
 
     }
 };
